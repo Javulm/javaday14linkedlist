0a3f49af1ec71ff2d50085021c026d28a99fc040
Javul Mulla <javulmulla8@gmail.com>
2022-08-18 19:46:45 +0530

    Added: insert number 30 between 56 & 70

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -2,18 +2,26 @@ package com.bridgelabz;
 
 public class LinkedListMain {
 	public static void main(String[] args) {
-		LinkedList<Integer> list1 = new LinkedList<>();
+		LinkedList list1 = new LinkedList();
 		list1.add(56);
 		list1.add(30);
 		list1.add(70);
 		list1.show();
 		System.out.println(" ");
 
-		LinkedList<Integer> list2 = new LinkedList<>();
+		LinkedList list2 = new LinkedList();
 		list2.push(70);
 		list2.push(30);
 		list2.push(56);
-		list1.show();
+		list2.show();
+		System.out.println(" ");
+		
+		LinkedList list3 = new LinkedList();
+		Node prevNode = list3.add(56);
+        list3.add(70);
+		list3.show();
+        list3.insertAfter(prevNode,  30);
+		list3.show();
 		System.out.println(" ");
 	}
 }
